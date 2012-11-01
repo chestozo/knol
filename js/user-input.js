1d30ce84e552429d286c531b7982fd4cfb4919f8
chestozo <chestozo@yandex-team.ru>
2012-11-01 18:16:27 +0400

    User input js code

---
+++ js/user-input.js
@@ -0,0 +1,6 @@
+// Parse number secure.
+function parseNumber(value, default_value) {
+    return isNaN(value = +value) ? default_value : value;
+}
+parseNumber('20', -1); // 20
+parseNumber('a20', -1); // 20
