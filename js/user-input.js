519e028002f2ee12d1f22607318b9fcac340d117
chestozo <romanok@bk.ru>
2012-11-01 18:17:37 +0400

    Example fix

--- js/user-input.js
+++ js/user-input.js
@@ -3,4 +3,4 @@ function parseNumber(value, default_value) {
     return isNaN(value = +value) ? default_value : value;
 }
 parseNumber('20', -1); // 20
-parseNumber('a20', -1); // 20
+parseNumber('a20', -1); // -1
