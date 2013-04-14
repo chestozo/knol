cb686a387e006f6b40e236626e3dc288a24a0129
chestozo <romanok@bk.ru>
2013-04-14 17:45:52 +0400

    Update collection.js

--- noscript/collection.js
+++ noscript/collection.js
@@ -14,9 +14,21 @@ no.Model.define('album', {
     
     //  how to extract models
     //  which events with that models must affect this one (for example: deleted model may affect collection)
-    extract models: {},
+    extract_models: {},
     
     //  caching params ? 
-    cache: {}
+    cache: {
+        fresh: '5m'  //  3s / 3sec / 5min,
+        mode: 'idle' //  timeout (after specified time we drop data no metter what)
+                     //  idle (if model was not touched for specified time — drop data)
+                     //  manual (on demand)
+    }
 
 }, no.Collection);
+
+/**Q:
+[ ] should we drop cache for data portions that were just got?
+    for example I surf some album and when I get to 15th page cache timeout is reached and we drop
+    all data (including the 14th page)
+[ ] when I request a photo A from the 0 page and then I request full 0 page should I check for photo A presense in cache?
+*/
